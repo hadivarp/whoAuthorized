d26530c534bc975c4ab3dbaa96700e4033aaed65
Hadi Varposhti <Hadi@Hadi-Varposhtis-iMac.local>
2024-01-14 16:44:38 +0300

    add signout

--- backend/app/controllers/authentications_controller.rb
+++ backend/app/controllers/authentications_controller.rb
@@ -29,8 +29,8 @@ class AuthenticationsController < ApplicationController
 
   def sign_in
     @user = User.find_by(email: user_params['email'])
-      if @user && @user.authenticate(user_params['password'])
-        @token = JsonWebToken.encode(user_id: @user.id)
+    if @user && @user.authenticate(user_params['password'])
+      @token = JsonWebToken.encode(user_id: @user.id)
       render json: {
         message: "User login successfully", user: { id: @user.id, email: @user.email }, token: @token
       }, status: :accepted
@@ -40,10 +40,15 @@ class AuthenticationsController < ApplicationController
   end
 
 
-  def sign_out
 
-    render json: { message: "User signed out successfully" }, status: :ok
+  def sign_out
+    if @current_user.update(token_valid: false)
+      render json: { message: 'User signed out successfully' }, status: :ok
+    else
+      render json: { error: 'Unable to sign out' }, status: :unprocessable_entity
+    end
   end
+
 
 
 
@@ -62,17 +67,31 @@ class AuthenticationsController < ApplicationController
   def authenticate_request
     header = request.headers['Authorization']
 
+    if header.blank?
+      render json: { error: 'Authorization header is missing' }, status: :unauthorized
+      return
+    end
+
+    logger.debug("Authorization header: #{header}")
+    logger.debug("Decoded token: #{@decode}")
+
     begin
+
       if header.present?
+        byebug
         @decode = JsonWebToken.decode(header)
+
         @current_user = User.find(@decode['user_id'])
       else
+        logger.error("Authorization header is missing.")
         head :unauthorized
       end
-    rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+    rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
+      logger.error("Authentication failed: #{e.message}")
       head :unauthorized
     end
   end
+
 
 
 end
